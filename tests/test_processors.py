cc5a34504a248de86430551023d35fc54e93f361
Ondřej Novák <ondrej.novak.2@cvut.cz>
2022-01-05 16:57:40 +0100

    WIP: add processors

---
+++ tests/test_processors.py
@@ -0,0 +1,6 @@
+# TODO:
+#  - single processor test
+#     - for all possible RichMessage configurations
+#     - for all possible sink/source variations
+#  - multi processor test
+#     - just test the topic matching
